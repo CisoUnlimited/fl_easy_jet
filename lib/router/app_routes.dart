e1d40113d9fcd2fcb91da94f993ecb297f8c4785
Manuel Zambudio Herrerías <manuel.zambudio.herrerias@gmail.com>
2025-05-06 13:47:38 +0200

    Empezamos con el routing

---
+++ lib/router/app_routes.dart
@@ -0,0 +1,82 @@
+import 'package:fl_components/models/models.dart';
+import 'package:flutter/material.dart';
+import 'package:fl_components/screens/screens.dart';
+
+class AppRoutes {
+  static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    MenuOption(
+      route: 'home',
+      icon: Icons.home_max_sharp,
+      name: 'Home Screen',
+      screen: const HomeScreen(),
+    ),
+    MenuOption(
+      route: 'listview1',
+      icon: Icons.list_alt,
+      name: 'Listview tipo 1',
+      screen: const Listview1Screen(),
+    ),
+    MenuOption(
+      route: 'listview2',
+      icon: Icons.list,
+      name: 'Listview tipo 2',
+      screen: const Listview2Screen(),
+    ),
+    MenuOption(
+      route: 'alert',
+      icon: Icons.add_alert_outlined,
+      name: 'Alertas',
+      screen: const AlertScreen(),
+    ),
+    MenuOption(
+      route: 'card',
+      icon: Icons.credit_card,
+      name: 'Tarjetas - Cards',
+      screen: const CardScreen(),
+    ),
+    MenuOption(
+      route: 'avatar',
+      icon: Icons.supervised_user_circle_outlined,
+      name: 'Circle Avatar',
+      screen: const AvatarScreen(),
+    ),
+    MenuOption(
+      route: 'animated',
+      icon: Icons.play_circle_outline_rounded,
+      name: 'Animated Container',
+      screen: const AnimatedScreen(),
+    ),
+    MenuOption(
+      route: 'inputs',
+      icon: Icons.input_rounded,
+      name: 'Forms: Inputs',
+      screen: const InputsScreen(),
+    ),
+    MenuOption(
+      route: 'slider',
+      icon: Icons.slideshow,
+      name: 'Slider - Slider',
+      screen: const SliderScreen(),
+    ),
+    MenuOption(
+      route: 'listviewbuilder',
+      icon: Icons.build_circle_outlined,
+      name: 'Listview Builder',
+      screen: const ListViewBuilderScreen(),
+    ),
+  ];
+
+  static Map<String, Widget Function(BuildContext)> getAppRoutes() {
+    Map<String, Widget Function(BuildContext)> appRoutes = {};
+    for (final option in menuOptions) {
+      appRoutes.addAll({option.route: (BuildContext context) => option.screen});
+    }
+    return appRoutes;
+  }
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    return MaterialPageRoute(builder: (context) => const AlertScreen());
+  }
+}
